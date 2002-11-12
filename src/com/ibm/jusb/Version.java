b8de2f5cb3b6417a9f15fe2b927fbe3d39bbdc76
Dan Streetman <ddstreet@us.ibm.com>
2002-11-12 14:48:44 +0000

    updated version

--- src/com/ibm/jusb/Version.java
+++ src/com/ibm/jusb/Version.java
@@ -52,5 +52,5 @@ public class Version
 	public static String getRiVersion() { return VERSION_PLATFORM_INDEPENDENT_RI; }
 
 	private static final String VERSION_API = "0.9.3";
-	private static final String VERSION_PLATFORM_INDEPENDENT_RI = "0.9.3";
+	private static final String VERSION_PLATFORM_INDEPENDENT_RI = "0.9.4";
 }
